a4851414b12380382eec454649fbfd5f1debb6bb
unknown <harshitha61011@gmail.com>
2024-02-11 14:55:09 +0530

    complete my portfolio

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Harshitha</h1>
+        <p className={styles.description}>
+          I'm a Software Engineer who is passionate in coding and want to secure a Full Stack Web developer position in a reputable
+          organization to expand my learning, knowledge, and skills.
+        </p>
+        <a href="mailto:harshitha61011@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
